19040bd622677da2fecdcd0d5ddbb4f0bbd4cc5a
unknown <krishna6240@gmail.com>
2021-11-03 11:59:56 +0000

    mc1

--- src/jira/TestAnand.java
+++ src/jira/TestAnand.java
@@ -3,6 +3,7 @@ package jira;
 public class TestAnand {
 
     public static void main(String[] args) {
+        System.out.println("hello");
 
     }
 }
---
+++ src/jira/TestKrishna.java
@@ -0,0 +1,11 @@
+package jira;
+
+public class TestKrishna {
+    public static void main(String[] args) {
+        TestKrishna obj = new TestKrishna();
+        obj.Test();
+    }
+    public void Test(){
+        System.out.println("Test Process");
+    }
+}
