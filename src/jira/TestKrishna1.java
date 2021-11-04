5f9add976b51c32678466cc2445e1191e2e8064d
unknown <krishna6240@gmail.com>
2021-11-04 11:10:58 +0000

    Third test can be done

--- src/jira/TestKrishna1.java
+++ src/jira/TestKrishna1.java
@@ -3,5 +3,6 @@ package jira;
 public class TestKrishna1 {
     public static void main(String[] args) {
         System.out.println("My second Test");
+        System.out.println("My Third test");
     }
 }
